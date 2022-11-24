7e3ee65a933a49778d9a33fd6907e4aa8da95bff
AgentOsiol <113114202+AgentOsiol@users.noreply.github.com>
2022-11-24 15:31:42 +0000

    added map collision and added light to player

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -8,14 +8,14 @@ public class CharacterController : MonoBehaviour
     float rotation = 0.0f;
     float camRotation = 0.0f;
     float rotationSpeed = 2.0f;
-    float camRotationSpeed = 1.5f;
+    float camRotationSpeed = 0.5f;
     GameObject cam;
     Rigidbody myRigidbody;
 
     bool isOnGround;
     public GameObject groundChecker;
     public LayerMask groundLayer;
-    public float jumpForce = 300.0f;
+    public float jumpForce = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
